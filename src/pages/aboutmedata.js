a2f170795f814d05cc104e2a0213ff81c2260806
Henrik Wiener <henrik.wiener@outlook.com>
2024-04-07 12:58:24 -0600

    next update

--- src/pages/aboutmedata.js
+++ src/pages/aboutmedata.js
@@ -1,47 +1,32 @@
 const projectsdata = [
 	{
 	description:
-        "Welcome to my second React application. This website is dedicated to software engineering, my passion for software engineering, and to my learning journey. My name is Dan Wiener and I am a software engineer; a passionate software engineer. I love making software solutions of all kinds, but one of the most exhilarating feelings is pushing the boundaries of my skills and learning a new technology. I also love that feeling when I've been immersing myself in deep problem solving for a while and come up with a good solution to solve a complex problem in code. The feeling of accomplishment I get from that is unrivaled, and it is an understatement to say I take a great amount of pride in my work."
-        // I am particularly fond of modern frameworks and modern technologies, but I love the opportunity to solve any
-        // kind of software problem, the larger, more important, and more complex, the better. I often waiver between
-        // unwaivering confidence and debilitating impostor syndrome depending on the day or the hour, but my commitment to getting better
-        // has never stopped, and I have a natural want-to-learn-it-all and I-want-to-be-the-best attitude which drives me forward and to surround myself with mentors and people
-        // better than myself whom I learn from, whom push me to be better, but also sadly whom I compare myself to
-        // and then feel profoundly sad. Such is the life of a programmer? I want to contribute something meaningful. I just
-        // want to contribute something meaningful; that's all. I find all facets of programming fascinating,
-        // but areas which particularly intrigue me are optimization and multithreading, which I deep dove into while
-        // attending the Microsoft Software and Systems Academy when I was able to drastically reduce runtime
-        // utilizing .NET tasks and multithreading techniques. This learning also helped me identify and solve a race condition on the job
-        // at my first software engineering role, which I was very proud of, and the knowledge of data structures and algorithms
-        // I gained from my relentless practice and endless nights solving algorithms has helped me optimize code for
-        // runtime improvements of up to 97%. That said, I am not perfect, and there are several facets of software engineering
-        // which I seek to improve in; these include clean code, the importance of which I have learned through experience on the job
-        // and dealing with other people's code. I have also long had an interest in web development and particularly modern 
-        // js frameworks such as React or languages such as TypeScript, so I'm currently seeking to improve my skills in this realm,
-        // but I also greatly enjoy .NET and C#. I want to become more well-rounded in every regard, and in preparation for a new possible opportunity,
-        // I have been studying cloud (ramping up my skills in AWS), as well as Java SpringBoot. I am currently
-        // building a project which includes Java SpringBoot and React which is this website, to demonstrate my learning
-        // and gain hands-on experience. Other categories which fascinate me are both front end and backend,
-        // as I love developing client-facing code and developing UI, and its associated callbacks, while I also greatly enjoy
-        // backend and developing APIs, etc. a love for which I fostered while building my own authentication protocol which mimics
-        // OAUTH during the Microsoft Software and Systems Academy. I just love making software that makes an impact on someone.
-        // Anyway, I could wax poetic all night, but I am running out of things to say. I am just a developer who loves code,
-        // who wants to support my family, and wants to constantly improve. I would love to work with you to take on a new challenge and
-        // support your business with my fierce dedication to being better and providing the best product possible. The satisfaction I feel from making
-        // someone the customer directly benefits from, that I poured heart and soul into, is unwaivering. For the last year, I have been working as a
-        // software engineer as a subcontractor for a large space/defense organization, which has given me experience working within the strict confines
-        // of a regimented set of legacy frameworks and tightly-guarded development practices. Anyway, one last thing about me:
-        // I come from a family of software engineers, but I don't live in their shadow. I try not to. At least. I am my own software engineer.
-        // That's why I didn't do softare engineering for so long. Because the bar they set was so high. They are so successful. There was just no
-        // way I could ever attain their level of success. Dad who was a PHD/professor of software engineering at a major university. Half brothers who are 
-        // successful developers in the Bay Area (that is an understatement - but I won't go into further detail). Engineers left and right and on both sides
-        // of the family, mother who is a software engineer, etc. It wasn't until I was 27-28, then, that I picked up an interest in software engineering
-        // and decided to learn it in my free time, when I instantly fell in love and realized my passion and what I have been missing this whole time. I see
-        // why people love software engineering. I love software engineering, and think with time I will be well-equipped for it, but for now, I'm
-        // just trying to be as good as I can be. As good as my brothers, which I won't ever be. P.S. I love you :) Thanks for reading my website!
-        // This site will detail my learning journey and sort of document learning artifacts I find, have found useful, or have used to bolster my knowledge
-        // and increase my competence as a software engineer. I hope you can gain value from this site as I gained value from these resources!"
-	}
+                "Welcome to my second React application. This website is dedicated to software engineering, my passion for it, and my learning journey. My name is Dan Wiener, and I am a software engineer who is passionate about creating software solutions of all kinds. One of the most exhilarating aspects of my work is pushing the boundaries of my skills and learning new technologies. When I've been deeply immersed in a complex problem for a long time, the sense of accomplishment I get from solving it and coming up with a good code solution for it is spectacular, and as any software engineer with a growth mindset, I take immense pride in my work and continuously improving."
+	},
+        {
+        description:
+                "I am particularly fond of modern frameworks and modern technologies, but I love the opportunity to solve any kind of software problem, the larger, more important, and more complex, the better. I often waiver between unshakeable confidence and debilitating impostor syndrome depending on the day or the hour, but my commitment to getting better has never stopped, and I have a natural want-to-learn-it-all and I-want-to-be-the-best attitude which drives me forward and to surround myself with mentors and people better than myself whom I learn from, whom push me to be better, but also sadly whom I compare myself to and then feel profoundly sad. Such is the life of a programmer?"
+        },
+        {
+        description:
+                "I want to contribute something meaningful. I just want to contribute something meaningful; that's all. I find all facets of programming fascinating, but areas which particularly intrigue me are optimization and multithreading, which I deep dove into while attending the Microsoft Software and Systems Academy when I was able to drastically reduce runtime utilizing .NET tasks and multithreading techniques. This learning also helped me identify and solve a race condition on the job at my first software engineering role, which I was very proud of, and the knowledge of data structures and algorithms I gained from my relentless practice and endless nights solving algorithms has helped me optimize code for runtime improvements of up to 97%."
+        },
+        {
+        description:
+                "That said, I am not perfect, and there are several facets of software engineering which I seek to improve in; these include clean code, the importance of which I have learned through experience on the job and dealing with other people's code. I have also long had an interest in web development and particularly modern js frameworks such as React or languages such as TypeScript, so I'm currently seeking to improve my skills in this realm, but I also greatly enjoy .NET and C#. I want to become more well-rounded in every regard, and in preparation for a new possible opportunity, I have been studying cloud (ramping up my skills in AWS), as well as Java SpringBoot."
+        },
+        {
+        description:
+                "I am currently building a project which includes Java SpringBoot and React which is this website, to demonstrate my learning and gain hands-on experience. Other categories which fascinate me are both front end and backend, as I love developing client-facing code and developing UI, and its associated callbacks, while I also greatly enjoy backend and developing APIs, etc. a love for which I fostered while building my own authentication protocol which mimics OAUTH during the Microsoft Software and Systems Academy. I just love making software that makes an impact on someone. Anyway, I could wax poetic all night, but I am running out of things to say. I am just a developer who loves code, who wants to support my family, and wants to constantly improve. I would love to work with you to take on a new challenge and support your business with my fierce dedication to being better and providing the best product possible."
+        },
+        {
+        description:
+                "The satisfaction I feel from making someone the customer directly benefits from, that I poured heart and soul into, is unwaivering. For the last year, I have been working as a software engineer as a subcontractor for a large space/defense organization, which has given me experience working within the strict confines of a regimented set of legacy frameworks and tightly-guarded development practices. Anyway, one last thing about me: I come from a family of software engineers, but I don't live in their shadow. I try not to. At least. I am my own software engineer. That's why I didn't do softare engineering for so long. Because the bar they set was so high. They are so successful. There was just no way I could ever attain their level of success. Dad who was a PHD/professor of software engineering at a major university. Half brothers who are successful developers in the Bay Area (that is an understatement - but I won't go into further detail)."
+        },
+        {
+        description:
+                "Engineers left and right and on both sides of the family, mother who is a software engineer, etc. It wasn't until I was 27-28, then, that I picked up an interest in software engineering and decided to learn it in my free time, when I instantly fell in love and realized my passion and what I have been missing this whole time. I see why people love software engineering. I love software engineering, and think with time I will be well-equipped for it, but for now, I'm just trying to be as good as I can be. As good as my brothers, which I won't ever be. P.S. I love you :) Thanks for reading my website! This site will detail my learning journey and sort of document learning artifacts I find, have found useful, or have used to bolster my knowledge and increase my competence as a software engineer. I hope you can gain value from this site as I gained value from these resources!"
+        }
 ];
 
 export default projectsdata
