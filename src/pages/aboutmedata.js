3f267f2b694e2e848faa61ad1df9eb663036e682
Dan Wiener <danwiener1992@gmail.com>
2024-08-28 11:27:33 -0600

    Update text.

--- src/pages/aboutmedata.js
+++ src/pages/aboutmedata.js
@@ -1,15 +1,15 @@
 const projectsdata = [
 	{
 	description:
-                "Welcome to my second React project, where I share my passion and journey in software engineering. With a relentless drive to make good, clean code, I relish complex challenges, find satisfaction in solving them, and am perpetually intrigued by concepts including optimization, multithreading, and generative ai. I am not perfect by any means, but here I hope to share learning resources, or wisdom I find helpful or have found helpful along the way. These resources have helped me grow in myriad ways as I continuously strive to learn, and attempt to surround myself with the best of the best. As Warren Buffet said, \"You will move in the direction of the people that you associate with. It's important to associate with people that are better than yourself.\""
+                "Test text."
 	},
         {
         description:
-                "My experience ranges from .NET to modern JavaScript frameworks, with a particular interest in clean code and web development. Currently, I'm enhancing my skills in cloud technologies and Java SpringBoot, reflected in this project, which combines Java SpringBoot and React. Having worked as a subcontractor in a large space/defense organization, I've adapted to various frameworks and practices. I come from a family of accomplished software engineers and, despite late entry into the field at 27, I've quickly developed a deep love for software engineering. This site is a testament to my learning, showcasing resources that have shaped my skills, aiming to inspire and aid others in their software engineering journey."
+                "Test text."
         },
         {
         description:
-                "I am particularly fond of modern frameworks and modern technologies, but I love the opportunity to solve any kind of software problem, the larger, more important, and more complex, the better. I often waiver between unshakeable confidence and debilitating impostor syndrome depending on the day or the hour, but my commitment to getting better has never stopped, and I have a natural want-to-learn-it-all attitude which drives me forward and to better, but yet still, I constantly compare myself to others and the insidious impostor syndrome, feelings of inferiority, worsen. Such is the life of a programmer?"
+                "Test text."
         }
 ];
 
